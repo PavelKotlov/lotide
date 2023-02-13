482220df1885bb7a33a278d5aeb550c272209ff6
PavelKotlov <pavelkotlov@gmail.com>
2023-02-13 14:57:12 -0800

    Refactored the countLetters.js file along with it's test file to ensure all tests run succesfully

--- test/countLetters.test.js
+++ test/countLetters.test.js
@@ -1,5 +1,25 @@
-// describe("", () => {
-//   it("", () => {
-    
-//   });
-// });
+const countLetters = require('../lib/countLetters');
+const assertEqual = require('../lib/assertEqual');
+
+describe("The function countletters", () => {
+  it("should return an object that lists all the letters in a string and their quantity and see if letter 'h' appears 4 times", () => {
+    const actual = countLetters("lighthouse in the house")['h'];
+    const expected = 4;
+
+    assertEqual(actual, expected);
+  });
+
+  it("should return undefined if the object returned does not contain the letter searched for", () => {
+    const actual = countLetters("lighthouse in the house")['d'];
+    const expected = undefined;
+
+    assertEqual(actual, expected);
+  });
+
+  it("should return null if the sentence provided is a falsy value", () => {
+    const actual = countLetters("");
+    const expected = null;
+
+    assertEqual(actual, expected);
+  });
+});
