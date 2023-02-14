86f10bc319fa186bb93892cb53a81531beba4654
PavelKotlov <pavelkotlov@gmail.com>
2023-02-13 17:31:14 -0800

    Refactored the tail.js file along with it's test file to ensure all tests run succesfully.

--- test/tail.test.js
+++ test/tail.test.js
@@ -1,5 +1,22 @@
-// describe("", () => {
-//   it("", () => {
-    
-//   });
-// });
+const tail = require('../lib/tail');
+const eqArrays = require('../lib/eqArrays');
+const assertEqual = require('../lib/assertEqual');
+
+
+describe("The function tail", () => {
+  it("should return true if the rest of an array minus the first element equal to the given array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const actual = eqArrays(tail(words), ["Lighthouse", "Labs"]);
+    const expected = true;
+
+    assertEqual(actual, expected);
+  });
+
+  it("should return false if the rest of an array minus the first element is not equal the given array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const actual = eqArrays(tail(words), ["Lighthouse"]);
+    const expected = false;
+
+    assertEqual(actual, expected);
+  });
+});
