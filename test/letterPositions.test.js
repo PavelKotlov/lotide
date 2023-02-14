c77bc0b469a1bffeb8ad9d5a8683ddcc9cd62fb7
PavelKotlov <pavelkotlov@gmail.com>
2023-02-13 16:54:28 -0800

    Refactored the letterPositions.js file along with it's test file to ensure all tests run succesfully.

--- test/letterPositions.test.js
+++ test/letterPositions.test.js
@@ -1,5 +1,18 @@
-// describe("", () => {
-//   it("", () => {
-    
-//   });
-// });
+const letterPositions = require('../lib/letterPositions');
+const assertArraysEqual = require('../lib/assertArraysEqual');
+
+describe("The function letterPositions", () => {
+  it("should return an array that return all the positions of a given letter within a sentence", () => {
+    const actual = letterPositions("lighthouse in the house").h;
+    const expected = [3, 5, 15, 18];
+
+    assertArraysEqual(actual, expected);
+  });
+
+  it("should return undefined if a given letter's postions are non existant since the letter is not present in the sentence", () => {
+    const actual = letterPositions("lighthouse in the house").p;
+    const expected = undefined;
+
+    assertArraysEqual(actual, expected);
+  });
+});
