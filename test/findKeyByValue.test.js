52d03530e7cd6e539f1f503318edfd6b89dbf185
PavelKotlov <pavelkotlov@gmail.com>
2023-02-13 16:16:50 -0800

    Refactored the finKeyByValue.js file along with it's test file to ensure all tests run succesfully.

--- test/findKeyByValue.test.js
+++ test/findKeyByValue.test.js
@@ -1,5 +1,30 @@
-// describe("", () => {
-//   it("", () => {
-    
-//   });
-// });
+const findKeyByValue = require('../lib/findKeyByValue');
+const assertEqual = require('../lib/assertEqual');
+
+describe("The function findKeyByValue", () => {
+  it("should return the value of an object key based on the value of the object parameter, 'The Wire' === 'drama'", () => {
+    const bestTVShowsByGenre = {
+      sci_fi: "The Expanse",
+      comedy: "Brooklyn Nine-Nine",
+      drama:  "The Wire"
+    };
+
+    const actual = findKeyByValue(bestTVShowsByGenre, "The Wire");
+    const expected = "drama";
+
+    assertEqual(actual, expected);
+  });
+
+  it("should return undefined if an object key's value cannot be returned because  the object parameter doesn't exist, 'That '70s Show' === undefined", () => {
+    const bestTVShowsByGenre = {
+      sci_fi: "The Expanse",
+      comedy: "Brooklyn Nine-Nine",
+      drama:  "The Wire"
+    };
+
+    const actual = findKeyByValue(bestTVShowsByGenre, "That '70s Show");
+    const expected = undefined;
+
+    assertEqual(actual, expected);
+  });
+});
