7d899bbdf70e1901ec1e4b607f6275d0158f58fc
PavelKotlov <pavelkotlov@gmail.com>
2023-02-13 20:36:14 -0800

    corrected the middle.js file along with it's test file to ensure all tests run succesfully using chai.

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,34 +1,16 @@
 const middle = require('../lib/middle');
-const eqArrays = require('../lib/eqArrays');
-const assertEqual = require('../lib/assertEqual');
+const assert = require('chai').assert;
 
-describe("The function middle", () => {
-  it("should return an empty array if a given array has 2 or less elements", () => {
-    const actual = eqArrays(middle([1, 2]), []);
-    const expected = true;
-
-    assertEqual(actual, expected);
+describe("#middle", () => {
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
   });
 
-  it("should return the middle element if a given array has an odd number of elements", () => {
-    const actual = eqArrays(middle([1, 2, 3]), [2]);
-    const expected = true;
-
-    assertEqual(actual, expected);
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
   });
 
-  it("should return the middle two elements if a given array has an even number of elements", () => {
-    const actual = eqArrays(middle([1, 2, 3, 4]), [2, 3]);
-    const expected = true;
-
-    assertEqual(actual, expected);
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
-
-  it("should return false if the given arrays are not equal", () => {
-    const actual = eqArrays(middle([1, 2, 3, 4]), [7, 8]);
-    const expected = false;
-
-    assertEqual(actual, expected);
-  });
-
 });
