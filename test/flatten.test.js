80b0a8effc435f4d45fa02bcb0c710026d6c7892
PavelKotlov <pavelkotlov@gmail.com>
2023-02-13 20:12:31 -0800

    corrected the flatten.js file along with it's test file to ensure all tests run succesfully using chai.

--- test/flatten.test.js
+++ test/flatten.test.js
@@ -1,19 +1,8 @@
 const flatten = require('../lib/flatten');
-const eqArrays = require('../lib/eqArrays');
-const assertEqual = require('../lib/assertEqual');
+const assert = require('chai').assert;
 
-describe("The function flatten", () => {
-  it("should return 'Assertion Passed: value1 === value2' after interpolating nested arrays into the parent array, if the parent array is equal the given array", () => {
-    const actual = eqArrays(flatten([1, 5, 4, [8, 9], 5, [7, 8]]), [1, 5, 4, 8, 9, 5, 7, 8]);
-    const expected = true;
-
-    assertEqual(actual, expected);
-  });
-
-  it("should return 'Assertion Failed: value1 !== value2' after interpolating nested arrays into the parent array, if the parent array is not equal the given array", () => {
-    const actual = eqArrays(flatten([1, 5, 4, [8, 9], 5, [7, 8]]), [1, 5, 4, [8, 9], 5, [7, 8]]);
-    const expected = false;
-
-    assertEqual(actual, expected);
+describe("#flatten", () => {
+  it("returns [1, 5, 4, 8, 9, 5, 7, 8] for [1, 5, 4, [8, 9], 5, [7, 8]]", () => {
+    assert.deepEqual(flatten([1, 5, 4, [8, 9], 5, [7, 8]]), [1, 5, 4, 8, 9, 5, 7, 8]);
   });
 });
