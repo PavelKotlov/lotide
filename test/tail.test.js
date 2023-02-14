925877f736bace0e1752fee2cc190b78cf7ea331
PavelKotlov <pavelkotlov@gmail.com>
2023-02-13 19:19:57 -0800

    corrected the tail.js file along with it's test file to ensure all tests run succesfully using chai.

--- test/tail.test.js
+++ test/tail.test.js
@@ -1,22 +1,9 @@
 const tail = require('../lib/tail');
-const eqArrays = require('../lib/eqArrays');
-const assertEqual = require('../lib/assertEqual');
+const assert = require('chai').assert;
 
 
-describe("The function tail", () => {
-  it("should return true if the rest of an array minus the first element equal to the given array", () => {
-    const words = ["Yo Yo", "Lighthouse", "Labs"];
-    const actual = eqArrays(tail(words), ["Lighthouse", "Labs"]);
-    const expected = true;
-
-    assertEqual(actual, expected);
-  });
-
-  it("should return false if the rest of an array minus the first element is not equal the given array", () => {
-    const words = ["Yo Yo", "Lighthouse", "Labs"];
-    const actual = eqArrays(tail(words), ["Lighthouse"]);
-    const expected = false;
-
-    assertEqual(actual, expected);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
 });
