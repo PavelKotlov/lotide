60f88241721e325ad5a247370ca38a176caa334b
PavelKotlov <pavelkotlov@gmail.com>
2023-02-13 20:05:55 -0800

    corrected the findKey.js file along with it's test file to ensure all tests run succesfully using chai.

--- test/findKey.test.js
+++ test/findKey.test.js
@@ -1,8 +1,8 @@
 const findKey = require('../lib/findKey');
-const assertEqual = require('../lib/assertEqual');
+const assert = require('chai').assert;
 
-describe("The function findKey", () => {
-  it("should return the first restaurant's name given a certain amount of michelin stars, 'noma' has 2 stars", () => {
+describe("#findKey", () => {
+  it("returns 'noma' for x => x.stars === 2", () => {
     const michelinStarRestaurants = {
       "Blue Hill": { stars: 1 },
       "Akaleri":   { stars: 3 },
@@ -12,13 +12,10 @@ describe("The function findKey", () => {
       "Akelarre":  { stars: 3 }
     };
 
-    const actual = findKey(michelinStarRestaurants, x => x.stars === 2);
-    const expected = "noma";
-
-    assertEqual(actual, expected);
+    assert.strictEqual(findKey(michelinStarRestaurants, x => x.stars === 2), "noma");
   });
 
-  it("should return null if no restaurant's star count equals a given certain amount of michelin stars, null has 4 stars", () => {
+  it("returns null for x => x.stars === 4", () => {
     const michelinStarRestaurants = {
       "Blue Hill": { stars: 1 },
       "Akaleri":   { stars: 3 },
@@ -28,10 +25,6 @@ describe("The function findKey", () => {
       "Akelarre":  { stars: 3 }
     };
 
-    const actual = findKey(michelinStarRestaurants, x => x.stars === 4);
-    const expected = null;
-
-    assertEqual(actual, expected);
+    assert.isNull(findKey(michelinStarRestaurants, x => x.stars === 4));
   });
-
 });
