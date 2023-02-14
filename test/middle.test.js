4d5fe1df2c0f52d1b55d00783acba10275ad8ebf
PavelKotlov <pavelkotlov@gmail.com>
2023-02-13 17:19:07 -0800

    Refactored the middle.js file along with it's test file to ensure all tests run succesfully.

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,5 +1,41 @@
-// describe("", () => {
-//   it("", () => {
-    
-//   });
-// });
+const middle = require('../lib/middle');
+const eqArrays = require('../lib/eqArrays');
+const assertEqual = require('../lib/assertEqual');
+
+describe("The function middle", () => {
+  it("should return an empty array if a given array has 2 or less elements", () => {
+    const actual = eqArrays(middle([1, 2]), []);
+    const expected = true;
+
+    assertEqual(actual, expected);
+  });
+
+  it("should return the middle element if a given array has an odd number of elements", () => {
+    const actual = eqArrays(middle([1, 2, 3]), [2]);
+    const expected = true;
+
+    assertEqual(actual, expected);
+  });
+
+  it("should return the middle two elements if a given array has an even number of elements", () => {
+    const actual = eqArrays(middle([1, 2, 3, 4]), [2, 3]);
+    const expected = true;
+
+    assertEqual(actual, expected);
+  });
+
+  it("should return false if the given arrays are not equal", () => {
+    const actual = eqArrays(middle([1, 2, 3, 4]), [7, 8]);
+    const expected = false;
+
+    assertEqual(actual, expected);
+  });
+
+});
+
+
+// assertArraysEqual(eqArrays(middle([1, 2]), []));
+// assertArraysEqual(eqArrays(middle([1, 2, 3]), [2]));
+// assertArraysEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]));
+// assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]));
+// assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]));
