44851948ce355433e79a991f2ba49a7add48da5b
PavelKotlov <pavelkotlov@gmail.com>
2023-02-13 16:09:03 -0800

    Refactored the finKey.js file along with it's test file to ensure all tests run succesfully.

--- test/findKey.test.js
+++ test/findKey.test.js
@@ -1,5 +1,37 @@
-// describe("", () => {
-//   it("", () => {
-    
-//   });
-// });
+const findKey = require('../lib/findKey');
+const assertEqual = require('../lib/assertEqual');
+
+describe("The function findKey", () => {
+  it("should return the first restaurant's name given a certain amount of michelin stars, 'noma' has 2 stars", () => {
+    const michelinStarRestaurants = {
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+
+    const actual = findKey(michelinStarRestaurants, x => x.stars === 2);
+    const expected = "noma";
+
+    assertEqual(actual, expected);
+  });
+
+  it("should return null if no restaurant's star count equals a given certain amount of michelin stars, null has 4 stars", () => {
+    const michelinStarRestaurants = {
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+
+    const actual = findKey(michelinStarRestaurants, x => x.stars === 4);
+    const expected = null;
+
+    assertEqual(actual, expected);
+  });
+
+});
