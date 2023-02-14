b3f9ed011231ad933aa4710988087e54ebd7327b
PavelKotlov <pavelkotlov@gmail.com>
2023-02-13 18:55:50 -0800

    Refactored the without.js file along with it's test file to ensure all tests run succesfully.

--- test/without.test.js
+++ test/without.test.js
@@ -1,5 +1,19 @@
-// describe("", () => {
-//   it("", () => {
+const without = require('../lib/without');
+const eqArrays = require('../lib/eqArrays');
+const assertArraysEqual = require('../lib/assertArraysEqual');
+
+describe("The function without", () => {
+  it("should return an array without a given element", () => {
+    const actual = eqArrays(without([1, 2, "3"], ["3"]), [1, 2]);
+    const expected = true;
     
-//   });
-// });
+    assertArraysEqual(actual, expected);
+  });
+
+  it("should return an array without any changes if not given an element", () => {
+    const actual = eqArrays(without([1, 2, "3"], []), [1, 2, "3"]);
+    const expected = true;
+    
+    assertArraysEqual(actual, expected);
+  });
+});
